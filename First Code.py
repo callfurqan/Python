60f2010edbef45fd26e19ca9e197b78e154c36cd
callfurqan <159055360+callfurqan@users.noreply.github.com>
2024-02-05 22:17:53 +0500

    Add files via upload

---
+++ First Code.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Furqan Riaz
+#
+# Created:     28/01/2024
+# Copyright:   (c) Furqan Riaz 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+name= input("Who is your Superhero")
+print(name)
